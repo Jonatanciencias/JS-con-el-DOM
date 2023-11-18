0410222bd93e2a86978805a7d051cf7af4a509e2
g4n1d <jonatanciencias@gmail.com>
2023-11-18 17:28:06 -0500

    6 primeros ejercicios con la consola

--- toppings-de-pizza/app.js
+++ toppings-de-pizza/app.js
@@ -1,5 +1,28 @@
-// ex01 Mostrar en consola el div con el id contenedor
-const contenedor = document.getElementById('contenedor');
-console.log(contenedor)
+// ex01 Mostrar en consola el div con el id contenedora
+// Solución:
+// const contenedor = document.getElementById('contenedor');
+// (console.log(contenedor);
 
-// ex02
+// ex02 Mostar en consola la estructura html del div con id contenedor
+// Solucion:
+// const contenedor = document.getElementById('contenedor');
+// console.log(contenedor.innerHTML);
+
+// Ex03 Mostrar el tipo de dato de la esctructura html
+// Solucion:
+// const contenedor = document.getElementById('contenedor');
+// console.log(typeof contenedor.innerHTML);
+
+// Ex04 Mostrar en consola el elemento con id titulo
+// Solucion:
+// const titulo = document.getElementById('titulo');
+// console.log(titulo);/
+
+// Ex05 Mostrar el tipo de dato que es el elemento titulo
+// Solucion:
+// const titulo = document.getElementById('titulo');
+// console.log(typeof titulo);
+
+// Ex06 Mostrar el texto contenido dentro del elemento titulo
+const titulo = document.getElementById('titulo');
+console.log(titulo.innerText);
