72808ac409e4598524cf5eedcc67755f8ef117cb
g4n1d <jonatanciencias@gmail.com>
2023-11-19 13:23:46 -0500

    ejercicios con class name en js

--- toppings-de-pizza/app.js
+++ toppings-de-pizza/app.js
@@ -26,15 +26,45 @@
 // console.log(typeof titulo);
 
 // Ex06 Mostrar el texto contenido dentro del elemento titulo
+// Solucion:
 // const titulo = document.getElementById('titulo');
 // console.log(titulo.innerText);
 
 // Ex 07 Mostrar el tipo de dato que contiene el elemento titulo
+// Solucion:
 // const titulo =document.getElementById('titulo');
 // console.log(typeof titulo.innerText);
 
 // Ex 08 Mostrar el nombre de la etiqueta del elemento titulo
+// Solucion:
 // const titulo = document.getElementById('titulo');
 // console.log(titulo.tagName);
 
 // SELECCIONAR ELEMENTOS POR CLASE
+
+// Ex 09 Seleccionar todos los elementos con la clase topping
+// Solucion:
+// const toppings = document.getElementsByClassName('topping');
+// console.log(toppings);
+
+// Ex 10 Mostrar cuantos elementos hay con la clase topping
+// Solucion:
+// const toppings = document.getElementsByClassName('topping')
+// console.log(toppings.length);
+
+// Ex11 Mostrar en consola el primer elemento (Aceitunas) de la clase topping
+// Solucion:
+// const toppings = document.getElementsByClassName('topping');
+// console.log(toppings[0]);
+
+// Ex 12
+// Solucion:
+
+// Ex 13
+// Solucion:
+
+// Ex 14
+// Solucion:
+
+// Ex 15
+// Solucion:
