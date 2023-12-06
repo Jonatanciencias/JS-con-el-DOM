bcbb69e55cd9a483c5f8f31e043dd1fc18a3ffa1
g4n1d <jonatanciencias@gmail.com>
2023-12-06 11:21:39 -0500

    referencia a los elementos del DOM en JS

---
+++ colores_RGB/app.js
@@ -0,0 +1,8 @@
+// referencia a los elementos del DOM
+const inputRojo = document.getElementById('rojo');
+const inputVerde = document.getElementById('verde');
+const inputAzul = document.getElementById('azul');
+
+function () {
+
+}
