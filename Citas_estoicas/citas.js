2f058d7f45b04218e543996fccea8379b6088b2a
g4n1d <jonatanciencias@gmail.com>
2023-12-07 23:34:43 -0500

    estilos de CSS y adicion del estilo en bootstrap para el boton

---
+++ Citas_estoicas/citas.js
@@ -0,0 +1,47 @@
+let citas = [
+    {
+        'autor': 'Zenón de Citio',
+        'texto': 'La virtud es la única cosa buena'
+    },
+    {
+        'autor': 'Séneca',
+        'texto': 'La vida es una preparación para la muerte'
+    },
+    {
+        'autor': 'Epicteto',
+        'texto': 'Nadie es libre si no es dueño de sí mismo'
+    },
+    {
+        'autor': 'Epicteto',
+        'texto': 'La naturaleza nos ha dado dos oídos, pero solo una boca, para que podamos escuchar más y hablar menos'
+    },
+    {
+        'autor': 'Marco Aurelio',
+        'texto': 'No vivas como si fueras a vivir diez mil años. La muerte te acecha. Mientras tanto, sé feli'
+    },
+    {
+        'autor': 'Marco Aurelio',
+        'texto': 'El tiempo es un río que fluye sin cesar, y ningún hombre puede detenerlo'
+    },
+    {
+        'autor': 'Sócrates',
+        'texto': 'La sabiduría comienza con la reflexión'
+    },
+    {
+        'autor': 'Séneca',
+        'texto': 'El hombre más rico no es aquel que tiene más, sino aquel que necesita menos.'
+    },
+    {
+        'autor': 'Séneca',
+        'texto': 'Lo que nos hace felices no es conseguir lo que deseamos, sino aprender a desear lo que tenemos'
+    },
+    {
+        'autor': 'Immanuel Kant',
+        'texto': 'La paciencia es la fortaleza del débil, y la impaciencia la debilidad del fuerte.'
+    },
+    {
+        'autor': 'Confucio',
+        'texto': 'El mejor guerrero es el que lucha contra sí mismo.'
+    },
+
+]
